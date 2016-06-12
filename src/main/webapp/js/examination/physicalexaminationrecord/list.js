1d9c1a04db025bf56f19657c785d8e3115366736
liwanzhong <736398983@qq.com>
2016-06-13 07:40:31 +0800

    报告页面增加用户信息

--- src/main/webapp/js/examination/physicalexaminationrecord/list.js
+++ src/main/webapp/js/examination/physicalexaminationrecord/list.js
@@ -64,7 +64,7 @@ $(function() {
 			//actionStr+='<a target="_blank" href="'+rootPath + '/examination/physicalExamination/result.shtml?recordid='+rowdata.id+'">检测值</a>&nbsp;&nbsp;&nbsp;';
 			actionStr+='<a target="_blank" href="'+rootPath + '/examination/physicalExamination/report.shtml?physicalExaminationRecordFormMap.id='+rowdata.id+'">查看报告</a>&nbsp;&nbsp;&nbsp;';
 			actionStr+='<a target="_blank" href="'+rootPath + '/examination/physicalExamination/sick_risk.shtml?physicalExaminationRecordFormMap.id='+rowdata.id+'">查看疾病风险评估</a>&nbsp;&nbsp;&nbsp;';
-			//actionStr+='<a href="'+rootPath + '/examination/physicalExamination/downloadReport.shtml?recordid='+rowdata.id+'">下载PDF报告</a>';
+			actionStr+='<a href="'+rootPath + '/examination/physicalExamination/downloadReport.shtml?recordid='+rowdata.id+'">下载PDF报告</a>';
 		}
 		return actionStr;
 	}
